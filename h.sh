68fd45fd273ab21282de23df3af63e69b53d8c39
arun fung <arunfung@gmail.com>
2021-11-28 22:37:18 +0800

    switch to fhs-install-v2ray

--- h.sh
+++ h.sh
@@ -239,10 +239,10 @@ v2ray_install(){
 	fi
 
 	mkdir -p /root/v2ray && cd /root/v2ray
-	wget -N --no-check-certificate https://install.direct/go.sh
+	wget -N --no-check-certificate https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 	
-	if [[ -f go.sh ]];then
-		bash go.sh --force
+	if [[ -f install-release.sh ]];then
+		bash install-release.sh --force
 		judge "安装 V2ray"
 	else
 		echo -e "${Error} ${RedBG} V2ray 安装文件下载失败，请检查下载地址是否可用 ${Font}"
